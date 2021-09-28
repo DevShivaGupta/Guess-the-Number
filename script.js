34d835fe1e52f2006d351955e51ae67a06acace1
Shiva Gupta <48547715+TeDDyDev@users.noreply.github.com>
2021-09-28 18:11:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 'use strict';
 
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
@@ -54,6 +45,8 @@ document.querySelector('.check').addEventListener('click', function () {
       // document.querySelector('.message').textContent = '💥 You lost the game!';
       displayMessage('💥 You lost the game!');
       document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#FF0000';
+      
     }
   }
 
